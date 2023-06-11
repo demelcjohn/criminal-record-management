60142eef1f69096426549db9ba002d984176716f
demelcjohn <demal.john007@gmail.com>
2023-06-11 14:41:26 +0530

    added left side

--- src/components/personalDetails.tsx
+++ src/components/personalDetails.tsx
@@ -1,6 +1,6 @@
 // import Demo from "./demo";
 
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import Image from "next/image";
 export default function PersonalDetails({ id, description, status }: any) {
   return (
@@ -13,7 +13,7 @@ export default function PersonalDetails({ id, description, status }: any) {
       alignItems="center"
     >
       <Box width="90%" height="90%" sx={{ padding: "5%", overflow: "hidden" }}>
-        <Grid container width="100%" height="30%" sx={{ bgcolor: "red" }}>
+        <Grid container width="100%" height="30%">
           <Grid
             item
             width="30%"
@@ -21,33 +21,51 @@ export default function PersonalDetails({ id, description, status }: any) {
             display="flex"
             justifyContent="center"
             alignItems="center"
-            sx={{ bgcolor: "blue" }}
           >
             <Image src="/photo.jpg" alt="Icon" width={100} height={100} />
           </Grid>
-          <Grid item width="70%" height="100%" sx={{ bgcolor: "green" }}>
-            <Grid
-              width="100%"
-              height="50%"
-              sx={{ bgcolor: "pink" }}
-              display="flex"
-              justifyContent="left"
-              alignItems="center"
+          <Grid
+            item
+            width="70%"
+            height="100%"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "left",
+              flexDirection: "column",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                alignItems: "left",
+                marginBottom: "5px",
+              }}
             >
-              <Typography>Hey</Typography>
-            </Grid>
-            <Grid
-              width="100%"
-              height="50%"
-              display="flex"
-              justifyContent="left"
-              alignItems="center"
-            >
-              <Typography>Hey</Typography>
-            </Grid>
+              <Typography>Name:</Typography>
+              <Typography>{"Demel C John"}</Typography>
+            </div>
+
+            <div style={{ display: "flex", alignItems: "left" }}>
+              <Typography>Identity Number:</Typography>
+              <Typography>{"636783"}</Typography>
+            </div>
           </Grid>
         </Grid>
-        <Grid width="100%" height="70%" sx={{ bgcolor: "yellow" }}></Grid>
+        <Grid width="100%" height="70%">
+          <TextField
+            multiline
+            rows={15}
+            InputProps={{
+              readOnly: true,
+            }}
+            value=""
+            sx={{
+              width: "100%",
+              bgcolor: "white",
+            }}
+          />
+        </Grid>
       </Box>
     </Box>
   );
