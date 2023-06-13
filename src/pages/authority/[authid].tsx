a5731c7d740ed300d37efdc86ef156a64509739d
demelcjohn <demal.john007@gmail.com>
2023-06-13 17:23:14 +0530

    took modal to another component

--- src/pages/authority/[authid].tsx
+++ src/pages/authority/[authid].tsx
@@ -1,20 +1,9 @@
+import AddCaseModal from "@/components/addCaseModal";
 import { Box, Button, Grid, Modal, TextField, Typography } from "@mui/material";
 import Image from "next/image";
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
 
 export default function Authority() {
   const [openModal, setOpenModal] = useState(true);
@@ -97,26 +86,7 @@ export default function Authority() {
                 >
                   Add Case
                 </Button>
-                <Modal
-                  open={openModal}
-                  onClose={handleClose}
-                  aria-labelledby="modal-modal-title"
-                  aria-describedby="modal-modal-description"
-                >
-                  <Box sx={style}>
-                    <Typography
-                      id="modal-modal-title"
-                      variant="h6"
-                      component="h2"
-                    >
-                      Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                      Duis mollis, est non commodo luctus, nisi erat porttitor
-                      ligula.
-                    </Typography>
-                  </Box>
-                </Modal>
+                <AddCaseModal open={openModal} setOpen={setOpenModal} />
                 <TextField
                   sx={{ width: "100%", bgcolor: "white" }}
                   style={{ marginTop: "10%" }}
