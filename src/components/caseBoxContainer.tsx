dd2af0efe649b9f012c0d0b5ff95fb22afb2fdf6
demelcjohn <demal.john007@gmail.com>
2023-06-22 23:22:16 +0530

    casecontainer deleted react query

--- src/components/caseBoxContainer.tsx
+++ src/components/caseBoxContainer.tsx
@@ -6,19 +6,7 @@ import { CasesContext } from "@/context/casesContext";
 import { useContext } from "react";
 export default function CaseBoxContainer() {
   const { caseIds, setCaseIds }: any = useContext(CasesContext);
-  const onSuccess = (data: any) => {
-    console.log("Perform fetching", data);
-  };
-  const onError = (error: any) => {
-    console.log("Perform error", error);
-  };
 
-  // const { isLoading, data, isError, error, isFetching, refetch } =
-  //   useCaseBoxData(onSuccess, onError);
-
-  // if (isLoading || isFetching) {
-  //   return <BeatLoader color="#000000" size={30} />;
-  // }
   return (
     <Box
       sx={{ width: "80%", height: "90%", bgcolor: "#8BE8D7" }}
@@ -27,14 +15,7 @@ export default function CaseBoxContainer() {
       fontFamily={"sans-serif"}
       overflow={"scroll"}
     >
-      {caseIds &&
-        caseIds.map((cases: any) => (
-          <CaseBox
-            id={cases.id}
-            description={cases.description}
-            status={cases.status}
-          />
-        ))}
+      {caseIds && caseIds.map((cases: any) => <CaseBox id={cases.case_id} />)}
     </Box>
   );
 }
