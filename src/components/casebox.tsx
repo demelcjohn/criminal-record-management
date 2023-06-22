24547644783548bb1f953f513cf4efd6d5ad1ac9
demelcjohn <demal.john007@gmail.com>
2023-06-22 23:22:31 +0530

    added fetch in casebox

--- src/components/casebox.tsx
+++ src/components/casebox.tsx
@@ -1,9 +1,39 @@
 // import Demo from "./demo";
 
 import { Box, Button } from "@mui/material";
+import axios from "axios";
 import { useEffect, useState } from "react";
-export default function CaseBox({ id, description, status }: any) {
+export default function CaseBox({ id }: any) {
   const [color, setColor] = useState("blue");
+
+  const fetchData = async () => {
+    try {
+      const storedData: any = localStorage.getItem("token");
+      const parsedData = JSON.parse(storedData);
+      const response: any = await axios.get(
+        "https://crm-back-end-jiaa-git-main-jjesvin21.vercel.app/api/cases/cases/" +
+          id,
+        {
+          headers: {
+            tocken: parsedData.tocken,
+            user: parsedData.user,
+            role: "citizen",
+          },
+        }
+      );
+
+      console.log("cases 6ABC:", response.data.data);
+
+      localStorage.setItem("token", JSON.stringify(response.data));
+    } catch (error) {
+      console.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   useEffect(() => {
     if (status === "ongoing") {
       setColor("#EB5E5E");
@@ -31,7 +61,7 @@ export default function CaseBox({ id, description, status }: any) {
           justifyContent="space-between"
           alignItems="center"
         >
-          {id} {description}
+          {id} {}
           <Button variant="contained" size="small" color="info">
             Status
           </Button>
