6f0080ae263217b98f190adffa1856232209408e
demelcjohn <demal.john007@gmail.com>
2023-06-24 12:12:56 +0530

    added grid

--- src/components/homeLogin.tsx
+++ src/components/homeLogin.tsx
@@ -44,6 +44,9 @@ export default function HomeLogin() {
   return !loading ? (
     <Grid
       item
+      direction={"column"}
+      alignItems="center"
+      justifyContent="center"
       p={{ sm: 2, md: 4, lg: 4 }}
       sx={{ width: "40%", height: "100%", bgcolor: "#000000" }}
     >
@@ -52,68 +55,75 @@ export default function HomeLogin() {
         <Image src="/crmtext.svg" alt="Icon" width={250} height={100} />
       </Stack>
       <Grid
-        container
-        padding={"10px"}
+        direction={"column"}
         alignItems="center"
         justifyContent="center"
-        sx={{ height: "10%" }}
+        sx={{ width: "100%", height: "100%" }}
       >
-        <Grid item xs={5} sx={{ height: "150%" }}>
-          <Typography sx={{ color: "#FFFFFF" }}>
-            Identification Number
-          </Typography>
-        </Grid>
-        <Grid item xs={7} sx={{ pl: 2, height: "150%" }}>
-          <TextField
-            sx={{
-              width: "100%",
-              marginBottom: "2%",
-              bgcolor: "#FFFFFF",
-            }}
-            size="small"
-            variant="outlined"
-            inputProps={{
-              style: {
-                width: "100%",
-              },
-            }}
-            onChange={UIDChangeHandler}
-          />
-        </Grid>
         <Grid
-          item
-          xs={5}
-          style={{ alignItems: "center" }}
-          sx={{ height: "150%" }}
+          container
+          padding={"10px"}
+          alignItems="center"
+          justifyContent="center"
+          sx={{ height: "10%" }}
         >
-          <Typography sx={{ color: "#FFFFFF" }}>Password</Typography>
-        </Grid>
-        <Grid item xs={7} sx={{ pl: 2, height: "150%" }}>
-          <TextField
-            type="password"
-            sx={{
-              width: "100%",
-              marginBottom: "2%",
-              bgcolor: "#FFFFFF",
-            }}
-            size="small"
-            variant="outlined"
-            inputProps={{
-              style: {
+          <Grid item xs={5} sx={{ height: "150%" }}>
+            <Typography sx={{ color: "#FFFFFF" }}>
+              Identification Number
+            </Typography>
+          </Grid>
+          <Grid item xs={7} sx={{ pl: 2, height: "150%" }}>
+            <TextField
+              sx={{
                 width: "100%",
-              },
-            }}
-            onChange={PasswordChangeHandler}
-          />
-        </Grid>
-        <Grid sx={{ height: "80%", width: "30%" }}>
-          <Button
-            variant="contained"
-            sx={{ width: "100%", height: "100%" }}
-            onClick={submitHandler}
+                marginBottom: "2%",
+                bgcolor: "#FFFFFF",
+              }}
+              size="small"
+              variant="outlined"
+              inputProps={{
+                style: {
+                  width: "100%",
+                },
+              }}
+              onChange={UIDChangeHandler}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={5}
+            style={{ alignItems: "center" }}
+            sx={{ height: "150%" }}
           >
-            Submit
-          </Button>
+            <Typography sx={{ color: "#FFFFFF" }}>Password</Typography>
+          </Grid>
+          <Grid item xs={7} sx={{ pl: 2, height: "150%" }}>
+            <TextField
+              type="password"
+              sx={{
+                width: "100%",
+                marginBottom: "2%",
+                bgcolor: "#FFFFFF",
+              }}
+              size="small"
+              variant="outlined"
+              inputProps={{
+                style: {
+                  width: "100%",
+                },
+              }}
+              onChange={PasswordChangeHandler}
+            />
+          </Grid>
+          <Grid sx={{ height: "80%", width: "30%" }}>
+            <Button
+              variant="contained"
+              sx={{ width: "100%", height: "100%" }}
+              onClick={submitHandler}
+            >
+              Submit
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
